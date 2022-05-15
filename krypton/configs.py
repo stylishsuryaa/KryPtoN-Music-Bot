204869000e36b1596c2ceb5865c90be6cd649e52
stylishsuryaa <83962899+stylishsuryaa@users.noreply.github.com>
2022-05-15 12:37:48 +0530

    Update configs.py

--- krypton/configs.py
+++ krypton/configs.py
@@ -3,10 +3,10 @@ HEROKU = True   # NOTE Make it false if you're not deploying on heroku.
 # NOTE these values are for heroku.
 if HEROKU:
     from os import environ
-    API_ID = int(environ["API_ID"])
-    API_HASH = environ["API_HASH"]
-    SUDO_CHAT_ID = environ["SUDO_CHAT_ID"]
-    SESSION_STRING = environ["SESSION_STRING"]
+    API_ID = int(environ["7086582"])
+    API_HASH = environ["5f87de0805ef1e47680954eb70aa43bd"]
+    SUDO_CHAT_ID = environ["1921122662"]
+    SESSION_STRING = environ["BQBVHdU_QgTUYhsch_Dh2Pw8M-9w6cmqgtt9IuLSVUWVKQ4_M8U311Pn8yVAPsQb6FX8TwsGpKwmgy8vQ6gQuoY9nDWlVz8zTewbgzJfxLkqySJRpets9KYvel9ECrevxQUFtLUPMpayt3RSLufN9xILNLuU0PuS0N8iogyKT5fpAYNesQqS2z_owzIowol6AddhbOxf3kyCTEEw4OQAjcRweLymiJy-QLsvyZK1TNu0JpxL1GLWW1DsRSig1P1ozpcbFE5fBtBE5CNks-zd9TzWnwTrxF1PmsOSqJeu0GyxZyOjMuGgvADELx0qozRj0AwGg0e2VMiWSZOzm3yGNb9pcoIBZgA"]
 
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
